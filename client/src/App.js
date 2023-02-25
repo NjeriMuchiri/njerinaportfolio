211fd223cd364ce6b14b135b6e6f03639fc97361
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-25 13:41:03 +0300

    added a nav page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Nav from './components/Nav'
+import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom';
 function App(){
     
-
   return(
     <Router>
        <Nav />
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../images/logo4.png'
+// import { Link } from 'react-router-dom';
+// import Logo from '../images/logo4.png'
 
 const Nav = () =>{
     
 
 return (
    
-    <div>
-        
+    <div className='container mx-auto gb-gray-200 rounded-xl shadow border p-8 m-10'>
+        <p className='text-3xl text-gray-700 font-bold mb-5'> NjeriMuchiri</p>
+        <p className='text-gray-500 text-lg'></p>
     </div>
 
   )
